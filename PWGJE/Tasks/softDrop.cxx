bf48bd792fa58b45be455c0431d876d30d874b4f
louisemillot <millot.louise1@orange.fr>
2025-04-24 15:39:23 +0200

    adding processMC in softdrop.cxx

--- PWGJE/Tasks/softDrop.cxx
+++ PWGJE/Tasks/softDrop.cxx
@@ -15,9 +15,11 @@
 #include "Framework/AnalysisTask.h"      // O2Physics header
 #include "Framework/AnalysisDataModel.h" // O2Physics header
 
-using namespace std;
-using namespace fastjet;
+using namespace o2;
 using namespace o2::framework;
+using namespace o2::constants::math;
+using namespace fastjet; 
+using namespace std;
 
 // Forward declaration to make things clearer
 ostream & operator<<(ostream &, const PseudoJet &);
@@ -25,7 +27,7 @@ ostream & operator<<(ostream &, const PseudoJet &);
 //----------------------------------------------------------------------
 struct SoftDropTask {
   // Define the input and output data
-  Inputs inputs{{"inputTracks", "TRACKS", "INPUT TRACKS"}};
+  
 
   // Define the parameters for SoftDrop
   Configurable<double> z_cut{"z_cut", 0.10, "Symmetry cut parameter for SoftDrop"};
@@ -40,12 +42,12 @@ struct SoftDropTask {
     cout << "SoftDrop groomer is: " << sd->description() << endl;
   }
 
-  void process(aod::Tracks const& tracks)
+  void processData(aod::Tracks const& tracks)
   {
     // Convert O2 tracks to FastJet PseudoJets
     vector<PseudoJet> event;
     for (auto& track : tracks) {
-      PseudoJet particle(track.px(), track.py(), track.pz(), track.e());
+      PseudoJet particle(track.px(), track.py(), track.pz(), track.energy(o2::constants::physics::MassPionCharged));
       event.push_back(particle);
     }
     cout << "# read an event with " << event.size() << " particles" << endl;
@@ -69,9 +71,47 @@ struct SoftDropTask {
       cout << "  mass drop(mu):           " << sd_jet.structure_of<contrib::SoftDrop>().mu() << endl;
     }
   }
+PROCESS_SWITCH(SoftDropTask, processData, "process task for data particles", true);
 
-  contrib::SoftDrop* sd;
+
+
+void processMC(soa::Join<aod::McParticles, aod::McCollisions> const& mcParticles)
+
+  {
+    // Convert O2 MC particles to FastJet PseudoJets
+    vector<PseudoJet> event;
+    for (auto& particle : mcParticles) {
+      PseudoJet pj(particle.px(), particle.py(), particle.pz(), particle.e());
+      event.push_back(pj);
+    }
+    cout << "# read an event with " << event.size() << " particles" << endl;
+
+    // Define the jet definition and cluster the event
+    JetDefinition jet_def(antikt_algorithm, R);
+    ClusterSequence cs(event, jet_def);
+    vector<PseudoJet> jets = sorted_by_pt(cs.inclusive_jets(ptmin));
+
+    // Apply SoftDrop to each jet
+    for (unsigned ijet = 0; ijet < jets.size(); ijet++) {
+      PseudoJet sd_jet = (*sd)(jets[ijet]);
+      cout << endl;
+      cout << "original    jet: " << jets[ijet] << endl;
+      cout << "SoftDropped jet: " << sd_jet << endl;
+
+      assert(sd_jet != 0); // because soft drop is a groomer (not a tagger), it should always return a soft-dropped jet
+
+      cout << "  delta_R between subjets: " << sd_jet.structure_of<contrib::SoftDrop>().delta_R() << endl;
+      cout << "  symmetry measure(z):     " << sd_jet.structure_of<contrib::SoftDrop>().symmetry() << endl;
+      cout << "  mass drop(mu):           " << sd_jet.structure_of<contrib::SoftDrop>().mu() << endl;
+    }
+  }
+
+PROCESS_SWITCH(SoftDropTask, processMC, "process task for MC particles", false);
+
+contrib::SoftDrop* sd;
+
 };
+
 
 //----------------------------------------------------------------------
 /// Overloaded jet info output
