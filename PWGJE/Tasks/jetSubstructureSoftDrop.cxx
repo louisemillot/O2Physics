93f04da17d4d58339dd5a29548d239f356329b6f
louisemillot <millot.louise1@orange.fr>
2025-05-14 17:17:41 +0200

    adding EventCount

--- PWGJE/Tasks/jetSubstructureSoftDrop.cxx
+++ PWGJE/Tasks/jetSubstructureSoftDrop.cxx
@@ -181,7 +181,7 @@ struct JetSubstructureTask {
       thetaVec.push_back(theta);
 
       if (z >= zCut * TMath::Power(theta / (jet.r() / 100.f), beta)) {
-         LOGF(info, " JetRadius : %f", jet.r()/ 100.f  );
+         LOGF(info, " JetRadius : %d", jet.r()/ 100.f  );
         if (!softDropped) {//if the splitting hasent been already softdropped softdrop=false
           zg = z;
           rg = theta;
@@ -256,7 +256,7 @@ struct JetSubstructureTask {
     registry.fill(HIST("h_jets"), 0.5);
 
     LOGF(info, "Number of collisions: %d", collisions.size());
-    
+
     for (auto& collision : collisions) {
       if (!jetderiveddatautilities::selectCollision(collision, eventSelectionBits)) {
         return; //ne lit pas la suite du code si le if statement n'est pas accepté
