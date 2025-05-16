0ed87fcd834032310eeeb65c504a675d5ce8b71e
louisemillot <millot.louise1@orange.fr>
2025-05-16 15:36:12 +0200

    track selection

--- PWGJE/Tasks/jetSubstructureSoftDrop.cxx
+++ PWGJE/Tasks/jetSubstructureSoftDrop.cxx
@@ -143,11 +143,6 @@ struct JetSubstructureTask {
   Preslice<aod::JetParticles> ParticlesPerMcCollision = aod::jmcparticle::mcCollisionId;
 
 
-
-  // Filter trackCuts = (aod::jtrack::pt >= trackQAPtMin && aod::jtrack::pt < trackQAPtMax && aod::jtrack::eta > trackQAEtaMin && aod::jtrack::eta < trackQAEtaMax);
-  // Filter particleCuts = (aod::jmcparticle::pt >= trackQAPtMin && aod::jmcparticle::pt < trackQAPtMax && aod::jmcparticle::eta > trackQAEtaMin && aod::jmcparticle::eta < trackQAEtaMax);
-  // Filter collisionFilter = (nabs(aod::jcollision::posZ) < vertexZCut && aod::jcollision::centrality >= centralityMin && aod::jcollision::centrality < centralityMax);
-
   template <bool isMCP, bool isSubtracted, typename T, typename U>
   void jetReclustering(T const& jet, U& splittingTable)
   {
