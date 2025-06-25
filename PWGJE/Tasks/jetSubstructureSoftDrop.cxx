29a59679e21e3aa3b0d5ff40f9ff6207e47d093e
louisemillot <millot.louise1@orange.fr>
2025-06-25 16:25:33 +0200

    adding filter for MCP collisions & changing the name for collision filter

--- PWGJE/Tasks/jetSubstructureSoftDrop.cxx
+++ PWGJE/Tasks/jetSubstructureSoftDrop.cxx
@@ -167,7 +167,8 @@ struct JetSubstructureTask {
   }
 
   // Filter trackCuts = (aod::jtrack::pt >= trackPtMin && aod::jtrack::pt < trackPtMax && aod::jtrack::eta > trackEtaMin && aod::jtrack::eta < trackEtaMax);
-  Filter eventCuts = (nabs(aod::jcollision::posZ) < vertexZCut && aod::jcollision::centrality >= centralityMin && aod::jcollision::centrality < centralityMax);
+  Filter collisionFilter = (nabs(aod::jcollision::posZ) < vertexZCut && aod::jcollision::centrality >= centralityMin && aod::jcollision::centrality < centralityMax);
+  Filter mcCollisionFilter = (nabs(aod::jmccollision::posZ) < vertexZCut && aod::jmccollision::centrality >= centralityMin && aod::jmccollision::centrality < centralityMax);
 
   Preslice<aod::JetTracks> TracksPerCollision = aod::jtrack::collisionId;
   Preslice<aod::JetTracksSub> TracksPerCollisionDataSub = aod::bkgcharged::collisionId;
