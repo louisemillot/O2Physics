bc3e9a03feca9985a147a0aaaaa6ced9675a6ff2
louisemillot <millot.louise1@orange.fr>
2025-10-23 11:02:52 +0200

    adding informations on matching

--- PWGJE/Tasks/jetSubstructureSoftDrop.cxx
+++ PWGJE/Tasks/jetSubstructureSoftDrop.cxx
@@ -350,7 +350,7 @@ struct JetSubstructureTask {
 
   // Filter trackCuts = (aod::jtrack::pt >= trackPtMin && aod::jtrack::pt < trackPtMax && aod::jtrack::eta > trackEtaMin && aod::jtrack::eta < trackEtaMax);
   Filter collisionFilter = (nabs(aod::jcollision::posZ) < vertexZCut && aod::jcollision::centFT0C >= centralityMin && aod::jcollision::centFT0C < centralityMax);
-  Filter mcCollisionFilter = nabs(aod::jmccollision::posZ)< vertexZCut;
+  Filter mcCollisionFilter = (nabs(aod::jmccollision::posZ) < vertexZCut && aod::jmccollision::centFT0C >= centralityMin && aod::jmccollision::centFT0C < centralityMax);
 
   Preslice<aod::JetTracks> TracksPerCollision = aod::jtrack::collisionId;
   Preslice<aod::JetTracksSub> TracksPerCollisionDataSub = aod::bkgcharged::collisionId;
