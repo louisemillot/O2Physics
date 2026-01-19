03db183e05295e405a649f161b8a91da36643b4e
louisemillot <millot.louise1@orange.fr>
2026-01-19 13:57:47 +0100

    adding functions in table producer

--- PWGJE/TableProducer/slimTablesProducer.cxx
+++ PWGJE/TableProducer/slimTablesProducer.cxx
@@ -48,13 +48,13 @@ struct SlimTablesProducer {
   }
   PROCESS_SWITCH(SlimTablesProducer, processCollision, "Produce slim collision table", true);
 
-  // void processMcCollision(aod::JetMcCollisions const& mccollisions)
-  // {
-  //   for (const auto& mccoll : mccollisions) {
-  //     slimMcCollisions(mccoll.posZ(), mccoll.centFT0M(), mccoll.weight(), mccoll.accepted(), mccoll.ptHard());
-  //   }
-  // }
-  // PROCESS_SWITCH(SlimTablesProducer, processMcCollision, "Produce slim mc collision table", true);
+  void processMcCollision(aod::JetMcCollisions const& mccollisions)
+  {
+    for (const auto& mccoll : mccollisions) {
+      slimMcCollisions(mccoll.posZ(), mccoll.centFT0M(), mccoll.weight(), mccoll.accepted(), mccoll.ptHard());
+    }
+  }
+  PROCESS_SWITCH(SlimTablesProducer, processMcCollision, "Produce slim mc collision table", false);
 
   // void processTracks(aod::Collisions::iterator const& collision,
   //                    aod::Tracks const& tracks)
@@ -78,13 +78,13 @@ struct SlimTablesProducer {
   }
   PROCESS_SWITCH(SlimTablesProducer, processTracks, "Produce slim track table", true);
 
-  // void processParticles(aod::JetParticles const& parts)
-  // {
-  //   for (const auto& p : parts) {
-  //     slimParticles(p.mcCollision(), p.pt(), p.eta(), p.phi());
-  //   }
-  // }
-  // PROCESS_SWITCH(SlimTablesProducer, processParticles, "Produce slim particles", true);
+  void processParticles(aod::JetParticles const& parts)
+  {
+    for (const auto& p : parts) {
+      slimParticles(p.mcCollision(), p.pt(), p.eta(), p.phi());
+    }
+  }
+  PROCESS_SWITCH(SlimTablesProducer, processParticles, "Produce slim particles", false);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
