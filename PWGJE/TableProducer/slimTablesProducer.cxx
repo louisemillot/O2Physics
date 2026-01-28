1b107bca0bdb9581727a533f7210a539b878c6a2
louisemillot <millot.louise1@orange.fr>
2026-01-28 14:59:47 +0100

    test

--- PWGJE/TableProducer/slimTablesProducer.cxx
+++ PWGJE/TableProducer/slimTablesProducer.cxx
@@ -13,38 +13,9 @@
 /// \brief Task to produce a reduced version of Tables for tracks, collisions, mcparticles and mccollisions.
 /// \author Millot Louise <louise.millot@cern.ch>
 
-#include "PWGDQ/DataModel/ReducedInfoTables.h"
-#include "PWGHF/DataModel/DerivedTables.h"
-#include "PWGHF/Utils/utilsBfieldCCDB.h"
-#include "PWGJE/Core/JetDQUtilities.h"
-#include "PWGJE/Core/JetDerivedDataUtilities.h"
-#include "PWGJE/Core/JetV0Utilities.h"
-#include "PWGJE/DataModel/EMCALClusters.h"
-#include "PWGJE/DataModel/EMCALMatchedCollisions.h"
-#include "PWGJE/DataModel/Jet.h"
-#include "PWGJE/DataModel/JetReducedData.h"
-#include "PWGJE/DataModel/SlimTables.h"
-#include "PWGLF/DataModel/mcCentrality.h"
-#include "PWGUD/Core/SGCutParHolder.h"
-#include "PWGUD/Core/SGSelector.h"
-
-#include "Common/CCDB/ctpRateFetcher.h"
-#include "Common/Core/RecoDecay.h"
-#include "Common/Core/Zorro.h"
-#include "Common/Core/trackUtilities.h"
-#include "Common/DataModel/Centrality.h"
-#include "Common/DataModel/CollisionAssociationTables.h"
-#include "Common/DataModel/EventSelection.h"
-#include "Common/DataModel/Multiplicity.h"
-#include "Common/DataModel/TrackSelectionTables.h"
-
-#include "CCDB/BasicCCDBManager.h"
-#include "DetectorsBase/Propagator.h"
 #include "Framework/ASoA.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Framework/AnalysisTask.h"
-#include "Framework/O2DatabasePDGPlugin.h"
-#include "ReconstructionDataFormats/Vertex.h"
 #include <CommonConstants/MathConstants.h>
 #include <DetectorsBase/MatLayerCylSet.h>
 #include <Framework/AnalysisHelpers.h>
@@ -54,40 +25,12 @@
 #include <ReconstructionDataFormats/DCA.h>
 
 #include <string>
-#include <vector>
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
 struct SlimTablesProducer {
-
-  HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
-  Configurable<int> nBinsPt{"nBinsPt", 200, "N bins in pT histo"};
-  Configurable<float> minPt{"minPt", 0.15, "min pT to save"};
-  Configurable<float> maxPt{"maxPt", 200.0, "max pT to save"};
-  // Configurable<float> maxDCA{"maxDCA", 0.1, "max DCA"};
-  Configurable<float> minEta{"minEta", -0.9, "min eta to save"};
-  Configurable<float> maxEta{"maxEta", 0.9, "max eta to save"};
-  Configurable<bool> skipUninterestingEvents{"skipUninterestingEvents", true, "skip collisions without particle of interest"};
-  Configurable<int> minTPCNClsCrossedRows{"minTPCNClsCrossedRows", 80, "min TPC crossed rows"};
-  Configurable<float> vertexZCut{"vertexZCut", 10.0f, "Accepted z-vertex range"};
-  Configurable<std::string> eventSelections{"eventSelections", "sel8", "Event selection"};
-
-  std::vector<int> eventSelectionBits;
-
-  void init(InitContext&)
-  {
-    const AxisSpec axisCounter{1, 0, +1, ""};
-    const AxisSpec axisPt{nBinsPt, 0, 10, "p_{T}"};
-    histos.add("h_collisions", "event status;event status;entries", {HistType::kTH1F, {{4, 0.0, 4.0}}});
-    histos.add("ptHistogram", "ptHistogram", kTH1F, {axisPt});
-    histos.add("hTracksPerCollision2D", "Tracks per collision;collisionId;N tracks", {HistType::kTH2F, {{1000, 0, 1000}, {500, 0, 500}}});
-    eventSelectionBits = jetderiveddatautilities::initialiseEventSelectionBits(static_cast<std::string>(eventSelections));
-  }
-
-  Produces<o2::aod::SlimCollisions> slimCollisions;
-  Produces<o2::aod::SlimTracks> slimTracks;
 
   void process(aod::Collisions::iterator const& collision,
                aod::Tracks const& tracks)
