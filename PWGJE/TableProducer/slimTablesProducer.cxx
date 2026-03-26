1b0e7b8b7a27309494d1a1ff049fe2003783ef3f
louisemillot <millot.louise1@orange.fr>
2026-03-26 11:28:16 +0100

    MCD Test

--- PWGJE/TableProducer/slimTablesProducer.cxx
+++ PWGJE/TableProducer/slimTablesProducer.cxx
@@ -42,7 +42,8 @@ namespace o2::aod
 {
 DECLARE_SOA_TABLE(SlimCollisions, "AOD", "SlimCollisions",
                   o2::soa::Index<>,
-                  o2::aod::collision::PosZ);
+                  o2::aod::collision::PosZ,
+                  o2::aod::collision::CollisionTime);
 using SlimCollision = SlimCollisions::iterator;
 DECLARE_SOA_TABLE(SlMcCollisions, "AOD", "SlMcCollisions",
                   o2::soa::Index<>,
@@ -173,7 +174,7 @@ struct SlimTablesProducer {
       return;
     }
     histos.fill(HIST("h_collisions"), 1.5);
-    slimCollisions(collision.posZ());
+    slimCollisions(collision.posZ(), collision.collisionTime());
     auto ts = collision.collisionTime();
     auto slimCollIndex = slimCollisions.lastIndex();
     LOG(INFO) << "Collision PbPB globalindex = " << collision.globalIndex();
@@ -296,7 +297,7 @@ struct SlimTablesProducer {
       }
       LOG(INFO) << "MC collision global ID = " << mcColl.globalIndex()
                 << " coll global ID = " << collision.globalIndex();
-      slimCollisions(collision.posZ());
+      slimCollisions(collision.posZ(), collision.collisionTime());
       auto slimCollIndex = slimCollisions.lastIndex();
       auto ts = collision.collisionTime();
       LOG(INFO) << "Collision pp globalindex = " << collision.globalIndex();
