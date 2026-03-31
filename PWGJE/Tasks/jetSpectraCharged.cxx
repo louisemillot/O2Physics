c018feeea58911fee1c3996266e76254f17db4be
louisemillot <millot.louise1@orange.fr>
2026-03-31 22:35:04 +0200

    histos

--- PWGJE/Tasks/jetSpectraCharged.cxx
+++ PWGJE/Tasks/jetSpectraCharged.cxx
@@ -278,6 +278,7 @@ struct JetSpectraCharged {
   Filter trackCuts = (aod::jtrack::pt >= trackPtMin && aod::jtrack::pt < trackPtMax && aod::jtrack::eta > trackEtaMin && aod::jtrack::eta < trackEtaMax);
   Filter eventCuts = (nabs(aod::jcollision::posZ) < vertexZCut);
   Filter mcEventCuts = (nabs(aod::jmccollision::posZ) < vertexZCut);
+  Preslice<aod::JetParticles> perMcCollisionParticles = aod::jmcparticle::mcCollisionId;
 
   template <typename TTracks, typename TJets>
   bool isAcceptedJet(TJets const& jet, bool mcLevelIsParticleLevel = false)
