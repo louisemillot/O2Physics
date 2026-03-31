96a1b5230a576df0dff58bcde22ade4d092d417f
louisemillot <millot.louise1@orange.fr>
2026-03-31 19:03:37 +0200

    add small groups

--- PWGJE/TableProducer/slimTablesProducer.cxx
+++ PWGJE/TableProducer/slimTablesProducer.cxx
@@ -288,7 +288,7 @@ struct SlimTablesProducer {
   // PROCESS_SWITCH(SlimTablesProducer, processMCP, "process mccollisions and mcparticles for MCD", false);
 
   void processMC(soa::Filtered<aod::JetCollisionsMCD> const& collisions,
-                 soa::SmallGroups<aod::JetMcCollisions> const&,
+                 aod::JetMcCollisions const&, // join the weight
                  soa::Filtered<aod::JetTracksMCD> const& tracks,
                  soa::Filtered<aod::JetParticles> const& particles)
   {
