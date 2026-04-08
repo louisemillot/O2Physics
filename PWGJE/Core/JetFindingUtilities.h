6947505384a5b51097dfce45f1ea5a24d8eaba00
louisemillot <millot.louise1@orange.fr>
2026-04-08 20:24:22 +0200

    test

--- PWGJE/Core/JetFindingUtilities.h
+++ PWGJE/Core/JetFindingUtilities.h
@@ -392,8 +392,8 @@ void analyseParticles(std::vector<fastjet::PseudoJet>& inputParticles, const std
     }
     fastjetutilities::fillTracks(particle, inputParticles, particle.globalIndex(), JetConstituentStatus::track, pdgParticle->Mass());
     nAccepted++;
-    totalAcceptedParticles += nAccepted;
   }
+  totalAcceptedParticles += nAccepted;
   LOG(INFO) << "total accepted particles: " << totalAcceptedParticles;
 }
 
